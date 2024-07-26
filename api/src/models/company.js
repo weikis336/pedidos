382046da6febad2c953228a9e7d83f4623aad79e
Weikis336 <alejandromg336@gmail.com>
2024-07-26 14:08:06 +0200

    index from pedidos

--- api/src/models/company.js
+++ api/src/models/company.js
@@ -1,59 +1,59 @@
 module.exports = function (sequelize, DataTypes) {
-    const Company = sequelize.define('Company',
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-          allowNull: false
-        },
-        commercialAddres: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        commercialName: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        fiscalAddres: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        fiscalName: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        vatNumber: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        createdAt: {
-          type: DataTypes.DATE
-        },
-        updatedAt: {
-          type: DataTypes.DATE
-        }
-      }, {
-        sequelize,
-        tableName: 'companies',
-        timestamps: true,
-        paranoid: true,
-        indexes: [
-          {
-            name: 'PRIMARY',
-            unique: true,
-            using: 'BTREE',
-            fields: [
-              { name: 'id' }
-            ]
-          }
-        ]
+  const Company = sequelize.define('Company',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      commercialAddres: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      commercialName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      fiscalAddres: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      fiscalName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      vatNumber: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        type: DataTypes.DATE
       }
-    )
-  
-    Company.associate = function (models) {
-     
+    }, {
+      sequelize,
+      tableName: 'companies',
+      timestamps: true,
+      paranoid: true,
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [
+            { name: 'id' }
+          ]
+        }
+      ]
     }
-  
-    return Company
+  )
+
+  Company.associate = function (models) {
+    
   }
+
+  return Company
+}
