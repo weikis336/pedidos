97e0af14f331d689f85944577f5a08f2dd5f4c7d
Weikis336 <alejandromg336@gmail.com>
2024-08-22 18:26:42 +0200

    commit

--- api/src/routes/admin-fingerprint-routes .js
+++ api/src/routes/admin-fingerprint-routes .js
@@ -1,13 +1,12 @@
 module.exports = (app) => {
+  const router = require('express').Router()
+  const controller = require('../controllers/admin/fingerprint-controller.js')
 
-    const router = require('express').Router()
-    const controller = require('../controllers/admin/fingerprint-controller.js')
-  
-    router.post('/', controller.create)
-    router.get('/', controller.findAll)
-    router.get('/:id', controller.findOne)
-    router.put('/:id', controller.update)
-    router.delete('/:id', controller.delete)
-  
-    app.use('/api/admin/fingerprints', router)
-  }
+  router.post('/', controller.create)
+  router.get('/', controller.findAll)
+  router.get('/:id', controller.findOne)
+  router.put('/:id', controller.update)
+  router.delete('/:id', controller.delete)
+
+  app.use('/api/admin/fingerprints', router)
+}
