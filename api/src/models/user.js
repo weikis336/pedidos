b95be7a938ffc21e18beda82824756ded340bd0f
Weikis336 <alejandromg336@gmail.com>
2024-09-04 11:57:31 +0200

    Cambios

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,49 +1,49 @@
 module.exports = function (sequelize, DataTypes) {
-    const User = sequelize.define('User',
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-          allowNull: false
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        createdAt: {
-          type: DataTypes.DATE
-        },
-        updatedAt: {
-          type: DataTypes.DATE
-        }
-      }, {
-        sequelize,
-        tableName: 'users',
-        timestamps: true,
-        paranoid: true,
-        indexes: [
-          {
-            name: 'PRIMARY',
-            unique: true,
-            using: 'BTREE',
-            fields: [
-              { name: 'id' }
-            ]
-          }
-        ]
+  const User = sequelize.define('User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        type: DataTypes.DATE
       }
-    )
-  
-    User.associate = function (models) {
-      User.hasMany(models.UserActivationToken, { as: 'userActivationToken', foreignKey: 'userId' })
-      User.hasMany(models.UserCredential, { as: 'userCredential', foreignKey: 'userId' })
-      User.hasMany(models.UserResetPasswordToken, { as: 'userResetPassword', foreignKey: 'userId' })
+    }, {
+      sequelize,
+      tableName: 'users',
+      timestamps: true,
+      paranoid: true,
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [
+            { name: 'id' }
+          ]
+        }
+      ]
     }
-  
-    return User
+  )
+
+  User.associate = function (models) {
+    User.hasMany(models.UserActivationToken, { as: 'userActivationToken', foreignKey: 'userId' })
+    User.hasMany(models.UserCredential, { as: 'userCredential', foreignKey: 'userId' })
+    User.hasMany(models.UserResetPasswordToken, { as: 'userResetPassword', foreignKey: 'userId' })
   }
+
+  return User
+}
